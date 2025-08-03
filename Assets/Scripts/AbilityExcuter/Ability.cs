d88213a10dba217d087d43b774959dbe4dc5e5da
kevzn <paledust@hotmail.com>
2025-08-03 14:31:16 +0800

    添加ability manager

---
+++ Assets/Scripts/AbilityExcuter/Ability.cs
@@ -0,0 +1,82 @@
+using System;
+using UnityEngine;
+
+[System.Serializable]
+public class AbilityData
+{
+    public string abilityName;
+    public Vector2Int highLightFrame;
+    public Color abilityColor;
+}
+public enum AbilityState
+{
+    Detached,   //挂起
+    Pending,    //载入
+    Working,    //生效中
+    Complete,   //结束
+}
+public class Ability : MonoBehaviour
+{
+    protected string abilityID;
+    protected AbilityController parent = null; //buff作用者
+    protected AbilityState buffState = AbilityState.Detached;//buff生命状态
+
+    public string m_abilityID => abilityID;
+    public bool IsPending => buffState == AbilityState.Pending;
+    public bool IsDone => buffState == AbilityState.Complete;
+
+    protected Action onBuffStart;
+    protected Action onBuffComplete;
+    protected Action onBuffRemoved;
+
+
+    public virtual void Initialize(AbilityController parent)
+    {
+        this.parent = parent;
+    }
+    //修改buff的状态
+    public void ChangeBuffState(AbilityState newState)
+    {
+        if (buffState == newState) return;
+        buffState = newState;
+        switch (buffState)
+        {
+            case AbilityState.Working:
+                onBuffStart?.Invoke();
+                AbilityBegin();
+                break;
+            case AbilityState.Complete:
+                onBuffComplete?.Invoke();
+                AbilityComplete();
+                break;
+            case AbilityState.Detached:
+                onBuffRemoved?.Invoke();
+                AbilityRemove();
+                break;
+            default:
+                break;
+        }
+    }
+    public virtual void RefreshAbility() { } //buff刷新后
+    public virtual void UpdateAbility() { } //buff生效中
+
+    protected virtual void AbilityBegin() { } //buff生效后
+    protected virtual void AbilityComplete() { } //buff销毁前
+    protected virtual void AbilityRemove() { } //buff销毁后
+
+    public Ability OnBuffStart(Action startCallback)
+    {
+        onBuffStart = startCallback;
+        return this;
+    }
+    public Ability OnBuffComplete(Action completeCallback)
+    {
+        onBuffComplete = completeCallback;
+        return this;
+    }
+    public Ability OnBuffRemoved(Action destroyCallback)
+    {
+        onBuffRemoved = destroyCallback;
+        return this;
+    }
+}
---
+++ Assets/Scripts/AbilityExcuter/AbilityController.cs
@@ -0,0 +1,105 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+
+public class AbilityController : MonoBehaviour
+{
+    [SerializeField] private List<AbilityData> abilities;
+    [SerializeField] private Dragable_Clock clock;
+    private Dictionary<string, Ability> activeAbilities;
+    private Action<Ability> onAbilityCreated;
+
+    void OnEnable()
+    {
+        EventHandler.E_OnTriggerAbility += OnTriggerAbility;
+        EventHandler.E_OnChargedAbility += OnChargedAbility;
+    }
+    void OnDisable()
+    {
+        EventHandler.E_OnTriggerAbility -= OnTriggerAbility;
+        EventHandler.E_OnChargedAbility -= OnChargedAbility;
+    }
+    void Start()
+    {
+        activeAbilities = new Dictionary<string, Ability>();
+    }
+    void OnChargedAbility(string abilityName)
+    {
+        Debug.Log($"Ability Charged: {abilityName}");
+        var ability = abilities.Find(a => a.abilityName == abilityName);
+        for (int i = ability.highLightFrame.x; i <= ability.highLightFrame.y; i++)
+        {
+            clock.HighLightFrame(i, ability.abilityColor);
+        }
+    }
+    void OnTriggerAbility(string abilityName)
+    {
+        Debug.Log($"Ability triggered: {abilityName}");
+        var ability = abilities.Find(a => a.abilityName == abilityName);
+    }
+
+    void Update()
+    {
+        if (activeAbilities == null) return;
+        foreach (var buff in activeAbilities.Values.ToList())
+        {
+            if (buff.IsPending)
+                buff.ChangeBuffState(AbilityState.Working);
+            if (buff.IsDone)
+                HandleAbilityComplete(buff);
+            else
+            {
+                buff.UpdateAbility();
+                if (buff.IsDone)
+                    HandleAbilityComplete(buff);
+            }
+        }
+    }
+    public void CleanUpAllAbility()
+    {
+        foreach (var ability in activeAbilities.Values.ToList())
+        {
+            HandleAbilityRemove(ability);
+        }
+        activeAbilities.Clear();
+    }
+    public bool TryAddBuffRaw(Ability ability)
+    {
+        ability.Initialize(this);
+        //Todo：通过Buff Manager创建Buff，并调用Buff Awake
+        if (activeAbilities == null)
+            activeAbilities = new Dictionary<string, Ability>();
+
+        if (activeAbilities.ContainsKey(ability.m_abilityID))
+        {
+            ability.RefreshAbility();
+            //Todo:同类型buff，触发刷新buff事件
+        }
+        else
+        {
+            activeAbilities.Add(ability.m_abilityID, ability);
+            ability.ChangeBuffState(AbilityState.Pending);
+        }
+        onAbilityCreated?.Invoke(ability);
+        return true;
+    }
+    public void RemoveAbility(string id)
+    {
+        if (activeAbilities.ContainsKey(id))
+        {
+            HandleAbilityRemove(activeAbilities[id]);
+        }
+    }
+    void HandleAbilityRemove(Ability ability)
+    {
+        activeAbilities.Remove(ability.m_abilityID);
+        ability.ChangeBuffState(AbilityState.Detached);
+    }
+    void HandleAbilityComplete(Ability ability)
+    {
+        activeAbilities.Remove(ability.m_abilityID);
+        ability.ChangeBuffState(AbilityState.Detached);
+    }
+}
